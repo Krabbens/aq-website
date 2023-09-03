020be51131771568f954b65ff4ba704ed2f69781
Krabbens <50266271+Krabbens@users.noreply.github.com>
2023-09-03 03:54:15 +0200

    color picker rewrite

--- aq/src/components/ColorPicker.vue
+++ aq/src/components/ColorPicker.vue
@@ -1,138 +1,134 @@
 <template>
-    <div class="relative">
-        <div class="fixed top-0 left-0 w-full h-screen bg-black opacity-0" v-if="isVisible" @click="hide"></div>
-        <transition name="pop">
-            <div v-if="isVisible" class="absolute w-[240px] border rounded bg-white shadow-md">
-                <div class="h-[260px] flex justify-center items-center text-white rounded-t" :style="{ background: color }">
-                </div>
-                <div class="p-[1.5rem] pt-4">
-                    <div class="relative rounded-full border mb-4">
-                        <input type="range" min="0" max="360" v-model="h" class="w-full appearance-none h-3 rounded-full cursor-pointer"
-                            :style="gradientH" />
-                    </div>
-                </div>
-            </div>
-        </transition>
-        <div class="w-12 h-12 mx-auto my-4 border-4 border-white shadow cursor-pointer hover:opacity-80"
-            :style="{ background: color }" @click="toggle"></div>
-    </div>
+    <fieldset class="flex px-2 py-2 shadow-lg items-center border rounded-lg">
+        <legend class="mr-2 text-md font-semibold">State {{ label.idx + 1 }}</legend>
+        <div class="w-8 h-8 rounded border shadow" :style="{ backgroundColor: color }"></div>
+        <input type="range" min="0" max="360" v-model="hue" class="mx-4 appearance-none w-48 h-1.5 rounded border"
+            style="background: linear-gradient(to right, hsl(0, 100%, 50%), hsl(60, 100%, 50%), hsl(120, 100%, 50%), hsl(180, 100%, 50%), hsl(240, 100%, 50%), hsl(300, 100%, 50%), hsl(0, 100%, 50%));">
+        <label class="ml-2">{{ colorInfo.name }}</label>
+    </fieldset>
 </template>
   
 <script>
+import axios from 'axios';
+
 export default {
-    props: ["initial"],
+    name: 'ColorPicker',
+    props: {
+        label: {
+            type: Object,
+            required: true
+        }
+    },
     data() {
         return {
-            isVisible: true,
-            h: 265,
-            s: 100,
-            l: 100
+            hue: 0,
+            colorInfo: {
+                name: 'Loading...',
+                hex: '#000000'
+            }
         };
     },
     computed: {
         color() {
-            const hsl = hsb2hsl(this.h / 360, this.s / 100, this.l / 100);
-            const colorString = `hsl(${hsl.h}, ${hsl.s}%, ${hsl.l}%)`;
-            this.handleColorChange({ color: colorString });  // Calling the internal method here
-            return colorString;
-        },
-        gradientH() {
-            const gradient = Array.from({ length: 7 }, (_, i) => {
-                const h = i * 60;
-                const hsl = hsb2hsl(h / 360, this.s / 100, this.l / 100);
-                return `hsl(${hsl.h}, ${hsl.s}%, ${hsl.l}%)`;
-            }).join(", ");
-
-            console.log('Gradient:', gradient);  // log the output
-            return { backgroundImage: `linear-gradient(to right, ${gradient})` };
-        },
+            return `hsl(${this.hue}, 100%, 50%)`;
+        }
     },
     methods: {
-        handleColorChange(colorData) {
-            // Handle the color change here
-            console.log("Color changed to:", colorData.color);
+        hsvToHex(h, s, v) {
+            // Ensure that h, s, and v are in the valid range
+            h = Math.max(0, Math.min(360, h));
+            s = Math.max(0, Math.min(100, s));
+            v = Math.max(0, Math.min(100, v));
+
+            // Convert HSV to RGB
+            const c = (v / 100) * (s / 100);
+            const x = c * (1 - Math.abs(((h / 60) % 2) - 1));
+            const m = (v / 100) - c;
+            let r, g, b;
+
+            if (h >= 0 && h < 60) {
+                r = c;
+                g = x;
+                b = 0;
+            } else if (h >= 60 && h < 120) {
+                r = x;
+                g = c;
+                b = 0;
+            } else if (h >= 120 && h < 180) {
+                r = 0;
+                g = c;
+                b = x;
+            } else if (h >= 180 && h < 240) {
+                r = 0;
+                g = x;
+                b = c;
+            } else if (h >= 240 && h < 300) {
+                r = x;
+                g = 0;
+                b = c;
+            } else {
+                r = c;
+                g = 0;
+                b = x;
+            }
+
+            // Convert RGB to HEX
+            const rHex = Math.round((r + m) * 255).toString(16).padStart(2, '0');
+            const gHex = Math.round((g + m) * 255).toString(16).padStart(2, '0');
+            const bHex = Math.round((b + m) * 255).toString(16).padStart(2, '0');
+
+            return `#${rHex}${gHex}${bHex}`;
         },
-        toggle() {
-            this.isVisible = !this.isVisible;
-        },
-        hide() {
-            this.isVisible = false;
+        async setColorInfo(newHue) {
+            let hexString = this.hsvToHex(newHue, 100, 100);
+            //convert to string
+
+            console.log(hexString);
+
+            let hex = hexString.replace('#', '');
+            let redHex = hex.substring(0, 2);
+            let greenHex = hex.substring(2, 4);
+            let blueHex = hex.substring(4, 6);
+
+            axios
+                .get(`https://api.color.pizza/v1/${redHex}${greenHex}${blueHex}`)
+                .then((response) => {
+                    // Update the color name and hex value from the API response
+                    this.colorInfo.name = response.data.paletteTitle;
+                    this.colorInfo.hex = response.data.colors[0].hex;
+                })
+                .catch((error) => {
+                    console.error('Error fetching color data:', error);
+                });
+        }
+    },
+    watch: {
+        hue(newHue) {
+            // Make a GET request to the Color API when the slider value changes
+            this.setColorInfo(newHue);
         }
     },
     mounted() {
-        this.h = Math.floor(Math.random() * 360);
+        // Make a GET request to the Color API when the component is mounted
+        this.setColorInfo(0);
     }
 };
-
-function hsb2hsl(h, s, b) {
-    const hsl = { h };
-    hsl.l = (2 - s) * b;
-    hsl.s = s * b;
-
-    if (hsl.l <= 1 && hsl.l > 0) {
-        hsl.s /= hsl.l;
-    } else {
-        hsl.s /= 2 - hsl.l;
-    }
-
-    hsl.l /= 2;
-
-    if (hsl.s > 1) hsl.s = 1;
-    if (!hsl.s > 0) hsl.s = 0;
-
-    hsl.h *= 360;
-    hsl.s *= 100;
-    hsl.l *= 100;
-
-    return hsl;
-}
 </script>
   
 <style scoped>
-.pop-enter-active,
-.pop-leave-active {
-    transition: transform .5s, opacity .5s;
-    transition-timing-function: cubic-bezier(.8, .3, 0.25, 1.75);
-    transform: scale(1);
-}
-
-.pop-enter,
-.pop-leave-active {
-    opacity: 0;
-    transform: scale(0);
-}
-
-/* For Webkit browsers */
 input[type=range]::-webkit-slider-thumb {
-    -webkit-appearance: none; /* Important to override default styles */
-    width: 8px;  /* Adjust as needed */
-    height: 36px; /* Adjust as needed */
+    -webkit-appearance: none;
+    /* Important to override default styles */
+    width: 8px;
+    /* Adjust as needed */
+    height: 24px;
+    /* Adjust as needed */
     background: #fff;
     cursor: pointer;
     border-radius: 20%;
     border: 1px solid #ddd;
-    box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.2); /* Example box shadow, adjust as desired */
-    margin-top: -15.5px; /* Adjust to vertically center the thumb with the track */
+    box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.2);
+    /* Example box shadow, adjust as desired */
+    /* Adjust to vertically center the thumb with the track */
 }
-
-/* For Firefox */
-input[type=range]::-moz-range-thumb {
-    width: 8px;  /* Adjust as needed */
-    height: 30px; /* Adjust as needed */
-    background: #fff;
-    border: 1px solid #ddd;
-    border-radius: 20%;
-    cursor: pointer;
-    box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.2); /* Example box shadow, adjust as desired */
-}
-
-/* Ensure the track is visible by adjusting its height */
-input[type=range]::-webkit-slider-runnable-track {
-    height: 5px;
-}
-
-input[type=range]::-moz-range-track {
-    height: 5px;
-}
-
 </style>
