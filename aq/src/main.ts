6679cad30345dc41db1c56b57d8b28b8ecf527e4
Krabbens <50266271+Krabbens@users.noreply.github.com>
2023-09-18 17:45:50 +0200

    toasts

--- aq/src/main.ts
+++ aq/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHome, faCogs, faAdjust, faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Toast from "vue-toastification";
 
 library.add(faHome, faCogs, faMoon, faSun); 
 
@@ -13,12 +14,15 @@ import router from './router'
 import './assets/style.css'
 import 'magic.css/dist/magic.min.css'
 import 'material-icons/iconfont/material-icons.css';
+import 'tw-elements';
+import "vue-toastification/dist/index.css";
 
 const app = createApp(App)
 
 app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.use(createPinia())
+app.use(Toast)
 app.use(router)
 
 app.mount('#app')
